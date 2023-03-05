35143fc8e0c3f264956a31acea07ecc438817200
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-05 22:00:40 +0900

    query clients fixup

--- ygka/query_clients/query_client_factory.py
+++ ygka/query_clients/query_client_factory.py
@@ -4,16 +4,16 @@ from ygka.models import LanguageModel, YGKAConfigModel
 class QueryClientFactory:
     '''Factory for creating query clients.'''
 
-    def __init__(self, config: YGKAConfigModel):
-        self.config = config
+    def __init__(self, config_model: YGKAConfigModel):
+        self.config_model = config_model
 
     def create(self, language_model: LanguageModel):
         '''Create a query client.'''
         if language_model == LanguageModel.REVERSE_ENGINEERED_CHATGPT:
             from .reverse_engineered_chatgpt_client import ReverseEngineeredChatGPTClient
-            return ReverseEngineeredChatGPTClient(config=self.config.chatgpt_config)
+            return ReverseEngineeredChatGPTClient(config=self.config_model.chatgpt_config)
         elif language_model == LanguageModel.OFFICIAL_CHATGPT:
             from .official_chatgpt_client import OfficialChatGPTClient
-            return OfficialChatGPTClient(openai_api_key=self.config.openai_api_key)
+            return OfficialChatGPTClient(openai_api_key=self.config_model.openai_api_key)
         else:
             raise ValueError('Invalid language model.')
