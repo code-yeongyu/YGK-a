f5e08af33b1b6fe0b637e44d6fe2ab0e634bb093
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-05 22:02:32 +0900

    query clients fixup

--- ygka/query_clients/query_client_factory.py
+++ ygka/query_clients/query_client_factory.py
@@ -7,12 +7,12 @@ class QueryClientFactory:
     def __init__(self, config_model: YGKAConfigModel):
         self.config_model = config_model
 
-    def create(self, language_model: LanguageModel):
+    def create(self):
         '''Create a query client.'''
-        if language_model == LanguageModel.REVERSE_ENGINEERED_CHATGPT:
+        if self.config_model.language_model == LanguageModel.REVERSE_ENGINEERED_CHATGPT:
             from .reverse_engineered_chatgpt_client import ReverseEngineeredChatGPTClient
             return ReverseEngineeredChatGPTClient(config=self.config_model.chatgpt_config)
-        elif language_model == LanguageModel.OFFICIAL_CHATGPT:
+        elif self.config_model.language_model == LanguageModel.OFFICIAL_CHATGPT:
             from .official_chatgpt_client import OfficialChatGPTClient
             return OfficialChatGPTClient(openai_api_key=self.config_model.openai_api_key)
         else:
